04cf7a69f7faa338219ec613a50e25b9cc3123d8
Kirill <okaydiscord@mail.ru>
2024-04-04 12:44:00 +0300

    авторизация переведена на резервные коды учетки пользователя

--- auth.py
+++ auth.py
@@ -1,12 +1,14 @@
+import json
+
 import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll
 
 # параметры сообщества
-APPTOKEN = "TOKEN FOR GROUP" # сюда ввести токен из сообщества
+APPTOKEN = "APPTOKEN"
 GROUP_ID = 224431580
 ALBUM_ID = 302234059
 
-app_id = 6287487
+client_id = 6287487
 client_secret = "QbYic1K3lEV5kTGiqlq2"
 login = 'LOGIN'  # сюда ввести логин аккаунта пользователя
 password = 'PASSWORD'  # сюда ввести пароль от аккаунта пользователя
@@ -18,9 +20,27 @@ def auth_handler():
     return key, remember_device
 
 
+# в файл codes.json из проекта надо вставить резервные коды для учетки пользователя
+# Надо сходить в Настройки - Безопасность и вход - Двухфакторная аутентификация - Резервные коды
+# Вставить коды в файл codes.json, удалив пробелы между цифрами
+def auth_handler_with_code():
+    with open('../codes.json', mode='r') as file:
+        res = json.loads(file.read())
+    if len(res) == 0:
+        print('Внимание, список кодов пуст, авторизация невозможна')
+        return '', False
+
+    key = res[0]
+    res.pop(0)
+    with open('../codes.json', mode='w') as file:
+        json.dump(res, file)
+    remember_device = True
+    return key, remember_device
+
+
 def get_user_vkapi():
-    vk_user_session = vk_api.VkApi(login, password, app_id=app_id, client_secret=client_secret,
-                                   auth_handler=auth_handler)
+    vk_user_session = vk_api.VkApi(login, password, app_id=client_id, client_secret=client_secret,
+                                   auth_handler=auth_handler_with_code, api_version='5.131')
     try:
         vk_user_session.auth(token_only=True)
     except vk_api.AuthError as error_msg:
